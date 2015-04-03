1e1a6d35cb5db57dcb0a1e07c674a842765160f8
Etienne Dublé <etienne.duble@imag.fr>
2015-04-03 13:51:45 +0000

    setup.py: Preparing version 0.3

--- server/setup.py
+++ server/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "walt-server",
-    version = "0.1",
+    version = "0.3",
     packages = find_packages(),
     install_requires = ['rpyc>=3.3','plumbum>=1.4.2','walt-common','ipaddress>=1.0.7'],
 
