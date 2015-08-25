848b55dbc239e300da653ef696a539bcd6f5e108
Etienne Dublé <etienne.duble@imag.fr>
2015-08-25 13:48:40 +0000

    Improved client config.
    We add comments in the json file ~/walt.conf in order to be able to
    describe each item in more details. Since it is not a standard
    feature in json, we manage these comments manually.
    We also ensure that the items will be printed in a logical order
    in the json file.
    We provide an attribute to specify is a given missing attribute
    should be prompted to the user (or if the default value should
    be chosen silently).

--- client/walt/client/config.py
+++ client/walt/client/config.py
@@ -1,55 +1,101 @@
-import json
+import json, re
 from os.path import expanduser
+from collections import OrderedDict
 
-CONF_ITEMS = dict(
-    server = dict(
+SINGLE_ITEM=1
+ONE_OR_MORE_ITEMS=2
+
+CONF_ITEMS = OrderedDict([
+    ('server', dict(
         desc='ip or hostname of walt server',
         itemtype=str,
-        default='localhost'
-    )
-)
+        default='localhost',
+        user_input=True
+    ))
+])
 
 def ask_config(key):
     infos = CONF_ITEMS[key]
     desc = infos['desc']
     default = infos['default']
     itemtype = infos['itemtype']
+    print
     print 'Missing configuration item:', desc
-    msg = 'please enter it now [%s]: ' % str(default)
+    if 'explain' in infos:
+        print infos['explain']
+    if default:
+        msg = 'please enter it now [%s]: ' % str(default)
+    else:
+        msg = 'please enter it now: '
     while True:
         value = raw_input(msg)
         if value.strip() == '':
-            value = default
-            break
+            if default:
+                value = default
+                print 'Selected value:', str(value)
+                break
+            else:
+                continue
         try:
             value = itemtype(value)
             break
         except:
             print "Bad input: expected type is '%s'" % str(itemtype)
-    print 'Selected value:', str(value)
     return value
 
+def get_config_file():
+    return expanduser('~/walt.conf')
+
+# Note: json comments are not allowed in the standard
+# and thus not handled in the json python module.
+# We handle them manually.
 def get_config():
-    config_file = expanduser('~/walt.conf')
+    config_file = get_config_file()
     conf = None
     modified = False
     try:
         with open(config_file) as f:
-            conf = json.load(f)
+            # filter out comments
+            filtered = re.sub('#.*', '', f.read())
+            # read valid json
+            conf = json.loads(filtered, object_pairs_hook=OrderedDict)
     except:
         pass
-    if not isinstance(conf, dict):
-        conf = {}
+    if not isinstance(conf, OrderedDict):
+        conf = OrderedDict()
         modified = True
     for key in CONF_ITEMS:
         if key not in conf:
-            conf[key] = ask_config(key)
+            if CONF_ITEMS[key]['user_input']:
+                conf[key] = ask_config(key)
+            else:
+                conf[key] = CONF_ITEMS[key]['default']
             modified = True
     if modified:
-        with open(config_file, 'w') as f:
-            json.dump(conf, f, indent=4)
-            print 'Configuration was stored in %s.' % config_file
+        save_config(conf)
+        print 'Configuration was stored in %s.' % config_file
     return conf
+
+def save_config(conf):
+    lines=[ ]
+    for k in conf:
+        # if not the 1st item add ',' before continuing
+        if len(lines) > 0:
+            lines = lines[0:-1] + [ lines[-1] + ',' ]
+        # add item label and description as 'json comments'
+        infos = CONF_ITEMS[k]
+        lines.append('')
+        lines.append('    # %s' % infos['desc'])
+        lines.append('    # %s' % re.sub('.', '-', infos['desc']))
+        if 'explain' in infos:
+            lines.extend([ '    # ' + line for line in infos['explain'].splitlines() ])
+        # get json output for this item only
+        # (by creating a temporary dictionary with just this item)
+        item_and_value = json.dumps({k:conf[k]}, indent=4).splitlines()[1:-1]
+        lines.extend(item_and_value)
+    # concatenate all and write the file
+    with open(get_config_file(), 'w') as f:
+        f.write('{' + '\n'.join(lines) + '\n}\n')
 
 conf = get_config()
 
