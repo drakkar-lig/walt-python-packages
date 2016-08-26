cfa3fb883416e728b4cfd4c98b4f6bfdeed9f441
Etienne Duble <etienne.duble@imag.fr>
2016-08-26 14:55:37 +0200

    versions-updater.py: fix api paths after refactoring

--- dev/version/versions-updater.py
+++ dev/version/versions-updater.py
@@ -50,8 +50,8 @@ from walt.common.versions import API_VERSIONING as CUR_API_VERSIONING, \
 # with what we had before and increment the api version
 # if it changed.
 API_LOCATIONS = {
-    'CS': [ 'walt.server.daemon.cs', 'walt.client.client' ],
-    'NS': [ 'walt.server.daemon.ns', 'walt.node.daemon' ]
+    'CS': [ 'walt.server.threads.main.api.cs', 'walt.client.client' ],
+    'NS': [ 'walt.server.threads.main.api.ns', 'walt.node.daemon' ]
 }
 
 new_api_versioning = CUR_API_VERSIONING.copy() # for now
