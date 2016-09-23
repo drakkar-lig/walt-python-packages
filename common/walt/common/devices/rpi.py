fa63e7c148b8e4bec1322b2c786ee7baaca53fd6
Etienne Duble <etienne.duble@imag.fr>
2016-09-23 11:06:33 +0200

    Nodes types: add rpi2 and rpi3

--- common/walt/common/devices/rpi.py
+++ common/walt/common/devices/rpi.py
@@ -1,12 +1,32 @@
 
+RPI_MAC_PREFIX = "b8:27:eb"
+
+def rpi_blink(b):
+    led_module = "heartbeat" if b else "mmc0"
+    with open('/sys/class/leds/led0/trigger', 'w') as f:
+        f.write("%s\n" % led_module)
+
 class RPi(object):
-    MAC_PREFIX = "b8:27:eb"
+    MAC_PREFIX = RPI_MAC_PREFIX
     SHORT_NAME = "rpi"
 
     @staticmethod
-    def blink(bool):
-        led_module = "heartbeat" if bool else "mmc0"
-        with open('/sys/class/leds/led0/trigger', 'w') as f:
-            f.write("%s\n" % led_module)
+    def blink(b):
+        rpi_blink(b)
 
+class RPi2(object):
+    MAC_PREFIX = RPI_MAC_PREFIX
+    SHORT_NAME = "rpi2"
+
+    @staticmethod
+    def blink(b):
+        rpi_blink(b)
+
+class RPi3(object):
+    MAC_PREFIX = RPI_MAC_PREFIX
+    SHORT_NAME = "rpi3"
+
+    @staticmethod
+    def blink(b):
+        rpi_blink(b)
 
