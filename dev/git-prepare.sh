3cb720d3ed534398639c8906141c81bcf29b6066
Etienne Duble <etienne.duble@imag.fr>
2016-10-28 16:33:28 +0200

    Added dev/git-prepare.sh

---
+++ dev/git-prepare.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+tty=$(tty)
+
+menu()
+{
+    echo "****************** MENU ****************************"
+    echo
+    select choice in "$@"
+    do
+        if [ ! -z "$choice" ]
+        then
+            echo "Selected: $choice"
+            return $REPLY
+        fi
+    done
+    return 0
+}
+
+handle_deleted()
+{
+    path="$1"
+    menu "Confirm deletion" "Restore file" "Pass file for now (^D)" "Stop (^C)"
+    case "$?" in
+        1)
+            git rm "$path"
+            ;;
+        2)
+            git checkout -- "$path"
+            ;;
+        3)
+            ;;
+        4)
+            SHOULD_STOP=1
+            ;;
+    esac
+}
+
+handle_modified()
+{
+    path="$1"
+    echo "****************** DIFF ****************************"
+    echo
+    git diff "$path"
+    echo
+    menu "Confirm all changes" "Select changes" "Bypass all changes for now (^D)" "Stop (^C)"
+    case "$?" in
+        1)
+            git add "$path"
+            ;;
+        2)
+            git add -p "$path"
+            ;;
+        3)
+            ;;
+        4)
+            SHOULD_STOP=1
+            ;;
+    esac
+}
+
+edit_file()
+{
+    if [ "$EDITOR" = "" ]
+    then
+        EDITOR="editor"
+    fi
+    "$EDITOR" "$1"
+}
+
+handle_untracked()
+{
+    path="$1"
+    if [ -d "$path" ]
+    then    # untracked directory
+        for file in $(find "$path" -type f)
+        do
+            echo "----> $file"
+            handle_untracked "$file"
+            if [ "$?" = "1" ]
+            then
+                break
+            fi
+        done
+    elif [ -f "$path" ]
+    then    # untracked file
+        loop=1
+        while [ $loop = 1 ]
+        do
+            if [ "$(grep -Il . "$path")" = "" ]
+            then
+                echo "Not displaying this file, since it seems to be binary."
+            else
+                echo "****************** NEW FILE CONTENT ****************"
+                echo
+                git diff /dev/null "$path"
+            fi
+            echo
+            menu "Confirm new file" "Edit file" "Bypass file for now (^D)" "Stop (^C)"
+            case "$?" in
+                1)
+                    git add "$path"
+                    loop=0
+                    ;;
+                2)
+                    edit_file "$path"
+                    ;;
+                3)
+                    loop=0
+                    ;;
+                4)
+                    loop=0
+                    SHOULD_STOP=1
+                    return 1
+                    ;;
+            esac
+        done
+    fi
+}
+
+SHOULD_STOP=0
+
+git status --porcelain | sed -e "s/^.//g" | grep -v '^ ' | \
+    while read state path
+    do
+        exec 6<&0   # save stdin (the pipe)
+        exec 0<$tty # direct stdin to the tty for interaction
+        clear
+        echo "****************** STATUS **************************"
+        echo
+        status="$(git -c color.status=always status)"
+        # git status: print staged changes
+        echo -n "$status" | sed -n '/Changes not staged.*/q;p'
+        # git status: print unstaged changes, with current file highlighted
+        echo -n "$status" | sed -n -e '/Changes not staged/,$ p' | \
+                    grep -E --color "(.*$path)?"
+        echo
+        case "$state" in
+            "M")
+                handle_modified $path
+                ;;
+            "D")
+                handle_deleted $path
+                ;;
+            "?")
+                handle_untracked $path
+                ;;
+            *)
+                echo 'Unknown state!'
+                exit
+                ;;
+        esac
+        exec 0<&6   # restore stdin (to the pipe)
+        if [ $SHOULD_STOP -gt 0 ]
+        then
+            break
+        fi
+    done
