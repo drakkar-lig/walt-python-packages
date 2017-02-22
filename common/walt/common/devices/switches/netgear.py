8a1353d9d599684801571b79aeb5e84e16c17e8a
Franck Rousseau <Franck.Rousseau@imag.fr>
2017-02-22 13:06:04 +0000

    netgear: detect netgear switches from new OUIs
    Today we know at least three different OUI for Netgear GS110TP
    switches.

--- common/walt/common/devices/switches/netgear.py
+++ common/walt/common/devices/switches/netgear.py
@@ -1,4 +1,5 @@
 NETGEAR_MAC_PREFIX = "6c:b0:ce"
+NETGEAR_MAC_PREFIXES = [ "6c:b0:ce", "28:c6:8e", "44:94:fc" ]
 
 class NetgearSwitch(object):
     MAC_PREFIX = NETGEAR_MAC_PREFIX
@@ -6,7 +7,8 @@ class NetgearSwitch(object):
     WALT_TYPE  = "switch"
     @staticmethod
     def is_such_a_device(vci, mac):
-        return mac.startswith(NETGEAR_MAC_PREFIX)
+        #return mac.startswith(NETGEAR_MAC_PREFIX)
+        return (len(filter(mac.startswith,NETGEAR_MAC_PREFIXES+[''])[0]) != 0)
 
 def get_device_classes():
     return (NetgearSwitch,)
