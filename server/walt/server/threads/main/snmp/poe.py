a4d3ce84fab779d2b31b5eb0b24bc7353bd1ad48
Etienne Dublé <etienne.duble@imag.fr>
2017-05-22 15:25:46 +0000

    LLDP vs PoE port indexing: be more tolerant.
    Switches do not use the same kind of indexing
    to list PoE ports and LLDP ports. In order to
    hard-reboot a node we have to know what is the
    PoE port, given the LLDP port it is connected to.
    We adopt a heuristic method for this.

--- server/walt/server/threads/main/snmp/poe.py
+++ server/walt/server/threads/main/snmp/poe.py
@@ -9,7 +9,7 @@ POE_PORT_SPEEDS=(10**7, 10**8, 10**9)   # 10Mb/s 100Mb/s 1Gb/s
 
 # Several mibs exist depending on the device vendor.
 # In the worst case, on the WalT platform we
-# could find devices of several vendors. 
+# could find devices of several vendors.
 # In this case we should unload a mib and load another
 # when we pass from one vendor to another.
 # In order to handle this, we use a wrapper around the
@@ -28,19 +28,26 @@ POE_PORT_MAPPING_CACHE = {}
 # index meaningful for the whole switch.
 # When we want to alter PoE state on a port, we have to compute
 # the appropriate (grp_index, port_index) given the LLDP port index.
-# I have found the following solution:
+# The following solution works on our stacked switches:
 # - list the switch ports with speed 10Mb/s 100Mb/s or 1Gb/s
 #   (these are the possible speeds for a PoE port), using IF-MIB
 # - list the PoE ports, using POWER-ETHERNET-MIB
 # - verify that these 2 lists have the same length
 # - "zip" these 2 lists in order to attach each LLDP port to its
 #   corresponding PoE port identification tuple.
-# IMPORTANT: This only works with switches where all ethernet
-# ports are PoE capable.
+# On netgear 8-port switches, this fails because unconnected ports
+# return a speed of 0 (and some other interfaces too (fiber, vlans)).
+# In this case, we check if the list of ports with type
+# 'ethernet CSMA/CD' is a continuous range. In this case,
+# we can probably consider the PoE ports are enumerated
+# the same way. IMPORTANT: This only works with switches where the
+# PoE capable ports are listed first.
 
 MSG_ISSUE_POE_PORT_MAPPING = """\
-WalT can only identify PoE ports on devices where all ports are PoE-capable.
-This does not seem to be the case on switch %s."""
+WalT could not guess how to correlate PoE ports and LLDP ports
+on switch %s. Sorry."""
+
+ETHERNETCSMACD = 6
 
 def get_poe_port_mapping(snmp_proxy, host):
     if not host in POE_PORT_MAPPING_CACHE:
@@ -49,13 +56,28 @@ def get_poe_port_mapping(snmp_proxy, host):
         iface_port_indexes = list(
                 int(k) for k, v in snmp_proxy.ifSpeed.items()
                 if v in POE_PORT_SPEEDS)
+        iface_type_indexes = list(
+                int(k) for k, v in snmp_proxy.ifType.items()
+                if int(v) == ETHERNETCSMACD)
         poe_port_indexes = list(
                 (int(grp_idx), int(grp_port)) \
                 for grp_idx, grp_port in snmp_proxy.pethPsePortAdminEnable.keys())
-        if len(iface_port_indexes) != len(poe_port_indexes):
+        # check if we have the same number of poe ports and 10/100/1000 ports
+        if len(iface_port_indexes) == len(poe_port_indexes):
+            # this probably means we can associate iface_port_indexes and
+            # poe_port_indexes one by one:
+            iface_to_poe_index = { a: b for a, b in \
+                    zip(iface_port_indexes, poe_port_indexes) }
+        # otherwise, check if we have a linear range of ethernet ports
+        # (i.e. there are no holes in those port indexes)
+        elif max(a-b for a, b in \
+                zip(iface_type_indexes[1:], iface_type_indexes[:-1])) == 1:
+            # this probably means we can associate iface_type_indexes and
+            # poe_port_indexes one by one:
+            iface_to_poe_index = { a: b for a, b in \
+                    zip(iface_type_indexes, poe_port_indexes) }
+        else:
             raise RuntimeError(MSG_ISSUE_POE_PORT_MAPPING % host)
-        iface_to_poe_index = { a: b for a, b in \
-                zip(iface_port_indexes, poe_port_indexes) }
         POE_PORT_MAPPING_CACHE[host] = iface_to_poe_index
     return POE_PORT_MAPPING_CACHE[host]
 
