b007b1542b8d6af2a221f0cff0693ef1ad199656
Etienne Dublé <etienne.duble@imag.fr>
2017-07-28 15:45:14 +0000

    Allow two forms of mac address for TFTP bootloaders

--- server/walt/server/threads/main/network/tftp.py
+++ server/walt/server/threads/main/network/tftp.py
@@ -12,11 +12,21 @@ def update(db):
     # each node must have a link from the tftp dir to
     # the walt image deployed on it. The name of this
     # link is the mac address of the node.
+    # For compatibility with different network bootloaders
+    # we actually provide 2 links, corresponding to 2 different
+    # ways to specify the mac address:
+    # <hh>:<hh>:<hh>:<hh>:<hh>:<hh>
+    # and
+    # <hh>-<hh>-<hh>-<hh>-<hh>-<hh>
     for db_node in db.select('nodes'):
-        image_path = get_mount_path(db_node.image)
-        failsafe_symlink(image_path, TFTP_PATH + db_node.mac, force_relative=True)
-        # this link is valid
-        invalid_links.discard(db_node.mac)
+        if db_node.image is not None:
+            image_path = get_mount_path(db_node.image)
+            mac_orig = db_node.mac
+            mac_dash = mac_orig.replace(':', '-')
+            for mac in (mac_orig, mac_dash):
+                failsafe_symlink(image_path, TFTP_PATH + mac, force_relative=True)
+                # this link is valid
+                invalid_links.discard(mac)
     # if there are still values in variable invalid_links,
     # we can remove the corresponding link
     for l in invalid_links:
