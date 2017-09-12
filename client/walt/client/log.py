07bf6bb0cdd50daa419fedb9f53f48e8fe730e36
Etienne Dublé <etienne.duble@imag.fr>
2017-09-12 12:03:53 +0000

    walt log wait: new command useful in scripts driving experiments

--- client/walt/client/log.py
+++ client/walt/client/log.py
@@ -74,6 +74,29 @@ you can create a checkpoint referencing a given date. For example:
 $ walt log add-checkpoint --date '%s' my-checkpoint
 """ % DATE_FORMAT_STRING_EXAMPLE)
 
+myhelp.register_topic('log-wait', """
+'walt log wait' allows to wait for a given log trace to be emitted.
+The command returns when it occurs.
+
+Option '--nodes' allows to specify the set of nodes monitored.
+If unspecified, the nodes you own (see walt --help-about node-terminology)
+will be selected.
+
+The default behavior (--mode ANY) of this command is to wait until a
+logline emitted by ONE of the selected nodes matches the specified
+regular expression.
+In some cases, one may require a different behavior by using option
+'--mode ALL'. With this option, the command will wait until ALL selected
+nodes emit a given logline.
+For example, let's consider an experiment run using scripts automatically
+started on node's bootup. These scripts send a logline "DONE" at the end
+of the experiment. In order to wait until all nodes are done with the
+experiment, the user can use:
+$ walt node wait --mode ALL --nodes node1,node2,node3 DONE
+This command will return when a logline including "DONE" has been
+received from all the specified experiment nodes.
+""")
+
 SECONDS_PER_UNIT = {'s':1, 'm':60, 'h':3600, 'd':86400}
 NUM_LOGS_CONFIRM_TRESHOLD = 1000
 
@@ -98,25 +121,14 @@ class WalTLog(cli.Application):
     """management of logs"""
     pass
 
-@WalTLog.subcommand("show")
-class WalTLogShow(cli.Application):
-    """Dump logs on standard output"""
+class WalTLogShowOrWait(cli.Application):
+    """Implements options and features common to "show" and "wait" subcommands"""
     format_string = cli.SwitchAttr(
                 "--format",
                 str,
                 argname = 'LOG_FORMAT',
                 default = DEFAULT_FORMAT_STRING,
                 help= """format used to print logs (see walt --help-about log-format)""")
-    realtime = cli.Flag(
-                "--realtime",
-                default = False,
-                help= """enable realtime mode (see walt --help-about log-realtime)""")
-    history_range = cli.SwitchAttr(
-                "--history",
-                str,
-                argname = 'HISTORY_RANGE',
-                default = 'none',
-                help= """history range to be retrieved (see walt --help-about log-history)""")
     set_of_nodes = cli.SwitchAttr(
                 "--nodes",
                 str,
@@ -126,19 +138,21 @@ class WalTLogShow(cli.Application):
     streams = cli.SwitchAttr(
                 "--streams",
                 str,
-                argname = 'REGEXP',
+                argname = 'STREAMS_REGEXP',
                 default = None,
                 help= """selected log streams (as a regular expr.)""")
 
-    def analyse_history_range(self, server, server_time):
+    @staticmethod
+    def analyse_history_range(server, history_range):
+        server_time = pickle.loads(server.get_pickled_time())
         MALFORMED=(False,)
         try:
-            self.history_range = self.history_range.lower()
-            if self.history_range == 'none':
+            history_range = history_range.lower()
+            if history_range == 'none':
                 return True, None
-            elif self.history_range == 'full':
+            elif history_range == 'full':
                 return True, (None, None)
-            parts = self.history_range.split(':')
+            parts = history_range.split(':')
             if len(parts) != 2:
                 return MALFORMED
             history = []
@@ -166,25 +180,70 @@ class WalTLogShow(cli.Application):
         except Exception as e:
             return MALFORMED
 
-    def main(self, logline_regexp = None):
-        if self.realtime == False and self.history_range == 'none':
-            print 'You must specify at least 1 of the options --realtime and --history.'
-            print "See 'walt --help-about log-realtime' and 'walt --help-about log-history' for more info."
-            return
-        for regexp in (self.streams, logline_regexp):
+    @staticmethod
+    def verify_regexps(*regexps):
+        for regexp in regexps:
             if regexp is None:
                 continue
             try:
                 re.compile(regexp)
             except:
                 print 'Invalid regular expression: %s.' % regexp
-                return
+                return False
+        return True
+
+    @staticmethod
+    def start_streaming(format_string, history_range, realtime, senders, streams,
+                        logline_regexp, stop_test):
+        conn = LogsFlowFromServer(conf['server'])
+        conn.request_log_dump(  history = history_range,
+                                realtime = realtime,
+                                senders = senders,
+                                streams = streams,
+                                logline_regexp = logline_regexp)
+        while True:
+            try:
+                record = conn.read_log_record()
+                if record == None:
+                    break
+                print format_string.format(**record)
+                sys.stdout.flush()
+                if stop_test is not None and stop_test(**record):
+                    break
+            except KeyboardInterrupt:
+                print
+                break
+            except Exception as e:
+                print 'Could not display the log record.'
+                print 'Verify your format string.'
+                break
+
+@WalTLog.subcommand("show")
+class WalTLogShow(WalTLogShowOrWait):
+    """Dump logs on standard output"""
+    realtime = cli.Flag(
+                "--realtime",
+                default = False,
+                help= """enable realtime mode (see walt --help-about log-realtime)""")
+    history_range = cli.SwitchAttr(
+                "--history",
+                str,
+                argname = 'HISTORY_RANGE',
+                default = 'none',
+                help= """history range to be retrieved (see walt --help-about log-history)""")
+
+    def main(self, logline_regexp = None):
+        if self.realtime == False and self.history_range == 'none':
+            print 'You must specify at least 1 of the options --realtime and --history.'
+            print "See 'walt --help-about log-realtime' and 'walt --help-about log-history' for more info."
+            return
+        if not WalTLogShowOrWait.verify_regexps(self.streams, logline_regexp):
+            return
         with ClientToServerLink() as server:
             senders = server.parse_set_of_nodes(self.set_of_nodes)
             if senders == None:
                 return
-            server_time = pickle.loads(server.get_pickled_time())
-            range_analysis = self.analyse_history_range(server, server_time)
+            range_analysis = WalTLogShowOrWait.analyse_history_range(server, self.history_range)
             if not range_analysis[0]:
                 print '''Invalid HISTORY_RANGE. See 'walt --help-about log-history' for more info.'''
                 return
@@ -195,26 +254,8 @@ class WalTLogShow(cli.Application):
                     print 'This will display approximately %d log records from history.' % num_logs
                     if not confirm():
                         return
-        conn = LogsFlowFromServer(conf['server'])
-        conn.request_log_dump(  history = history_range,
-                                realtime = self.realtime,
-                                senders = senders,
-                                streams = self.streams,
-                                logline_regexp = logline_regexp)
-        while True:
-            try:
-                record = conn.read_log_record()
-                if record == None:
-                    break
-                print self.format_string.format(**record)
-                sys.stdout.flush()
-            except KeyboardInterrupt:
-                print
-                break
-            except Exception as e:
-                print 'Could not display the log record.'
-                print 'Verify your format string.'
-                break
+        WalTLogShowOrWait.start_streaming(self.format_string, history_range, self.realtime,
+                                            senders, self.streams, logline_regexp, None)
 
 @WalTLog.subcommand("add-checkpoint")
 class WalTLogAddCheckpoint(cli.Application):
@@ -254,3 +295,48 @@ class WalTLogListCheckpoints(cli.Application):
     def main(self):
         with ClientToServerLink() as server:
             server.list_checkpoints()
+
+@WalTLog.subcommand("wait")
+class WalTLogWait(WalTLogShowOrWait):
+    """Wait for a given log line"""
+    mode = cli.SwitchAttr(
+                "--mode",
+                cli.Set("ALL", "ANY", case_sensitive = False),
+                argname = 'MODE',
+                default = 'ANY',
+                help= """specify mode (see walt --help-about log-wait)""")
+    time_margin = cli.SwitchAttr(
+                "--time-margin",
+                int,
+                argname = 'SECONDS',
+                default = 0,
+                help= """also look in recent past logs if they matched""")
+
+    def main(self, logline_regexp):
+        if not WalTLogShowOrWait.verify_regexps(self.streams, logline_regexp):
+            return
+        with ClientToServerLink() as server:
+            senders = server.parse_set_of_nodes(self.set_of_nodes)
+            if senders == None:
+                return
+            if self.time_margin != 0:
+                history_range = '-%ds:' % self.time_margin
+                range_analysis = WalTLogShowOrWait.analyse_history_range(server, history_range)
+                history_range = range_analysis[1]
+            else:
+                history_range = None
+        if self.mode == 'ANY':
+            # as soon as a logline matches, we stop
+            def stop_test(**record):
+                return True
+        else:
+            # we stop when all nodes have emitted a matching logline
+            missing_senders = set(senders)
+            def stop_test(**record):
+                missing_senders.discard(record['sender'])
+                if len(missing_senders) == 0:
+                    return True     # yes, we should stop
+                else:
+                    return False    # no, we are not done yet
+        WalTLogShowOrWait.start_streaming(self.format_string, history_range, True,
+                                    senders, self.streams, logline_regexp, stop_test)
