drakkar-lig/walt-python-packages

commit 14c378a9e14bedda202d017f34a0762302ea4853

walt log show: fix count_logs() when specifying streams

server/walt/server/threads/main/server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import re
 from walt.common.constants import WALT_SERVER_TCP_PORT
 from walt.common.devices.registry import get_device_cls_from_vci_and_mac
 from walt.common.tcp import TCPServer
@@ -140,3 +140,25 @@ class Server(object):
             return  # error already reported
         self.nodes.forget_vnode(info.mac)
         self.forget_device(name)
+
+    def count_logs(self, history, streams = None, senders = None, **kwargs):
+        unpickled_history = (pickle.loads(e) if e else None for e in history)
+        if streams:
+            # compute streams ids whose name match the regular expression
+            stream_ids = []
+            streams_re = re.compile(streams)
+            for row in self.db.get_logstream_ids(senders):
+                matches = streams_re.findall(row.name)
+                if len(matches) > 0:
+                    stream_ids.append(str(row.id))
+            if len(stream_ids) == 0:
+                return 0    # no streams => no logs
+            # we can release the constraint on senders since we restrict to
+            # their logstreams (cf. stream_ids variable we just computed)
+            senders = None
+        else:
+            stream_ids = None
+        return self.db.count_logs(history = unpickled_history,
+                                  senders = senders,
+                                  stream_ids = stream_ids,
+                                  **kwargs)

client/walt/client/log.py
@@ -100,6 +100,9 @@ received from all the specified experiment nodes.
 SECONDS_PER_UNIT = {'s':1, 'm':60, 'h':3600, 'd':86400}
 NUM_LOGS_CONFIRM_TRESHOLD = 1000
 
+def isatty():
+    return sys.stdout.isatty() and sys.stdin.isatty()
+
 def validate_checkpoint_name(name):
     return re.match('^[a-zA-Z0-9]+[a-zA-Z0-9\-]+$', name)
 
@@ -248,8 +251,11 @@ class WalTLogShow(WalTLogShowOrWait):
                 print '''Invalid HISTORY_RANGE. See 'walt --help-about log-history' for more info.'''
                 return
             history_range = range_analysis[1]
-            if history_range:
-                num_logs = server.count_logs(history = history_range, senders = senders)
+            # Note : if a regular expression is specified, we do not bother computing the number
+            # of log records, because this computation would be too expensive, and the number of
+            # matching lines is probably low.
+            if history_range and logline_regexp is None and isatty():
+                num_logs = server.count_logs(history = history_range, senders = senders, streams = self.streams)
                 if num_logs > NUM_LOGS_CONFIRM_TRESHOLD:
                     print 'This will display approximately %d log records from history.' % num_logs
                     if not confirm():

server/walt/server/postgres.py
@@ -139,14 +139,17 @@ class PostgresDB():
                     values)
         return self.c.rowcount  # number of rows updated
 
-    # allow statements like:
-    # mem_db.select("network", ip=ip)
-    def select(self, table, **kwargs):
+    def select_no_fetch(self, table, **kwargs):
         cols, values = self.get_cols_and_values(table, kwargs)
         where_clause = self.get_where_clause_pattern(cols)
         sql = "SELECT * FROM %s %s;" % (table, where_clause)
         self.c.execute(sql, values)
-        return self.c.fetchall()
+        return self.c
+
+    # allow statements like:
+    # mem_db.select("network", ip=ip)
+    def select(self, table, **kwargs):
+        return self.select_no_fetch(table, **kwargs).fetchall()
 
     # same as above but expect only one matching record
     # and return it.

server/walt/server/threads/main/api/cs.py
@@ -113,9 +113,8 @@ class CSAPI(APISession):
         context.server.set_image(context.requester.sync, node_set, image_tag)
 
     @api_expose_method
-    def count_logs(self, context, history, **kwargs):
-        unpickled_history = (pickle.loads(e) if e else None for e in history)
-        return context.server.db.count_logs(history = unpickled_history, **kwargs)
+    def count_logs(self, context, **kwargs):
+        return context.server.count_logs(**kwargs)
 
     @api_expose_method
     def forget(self, context, device_name):

server/walt/server/threads/main/db.py
@@ -78,12 +78,27 @@ class ServerDB(PostgresDB):
         sql, args = self.format_logs_query('count(*)', **kwargs)
         return self.execute(sql, args).fetchall()[0][0]
 
+    def get_logstream_ids(self, senders):
+        if senders == None:
+            return self.select_no_fetch('logstreams')
+        else:
+            sender_names = '''('%s')''' % "','".join(senders)
+            sql = """   SELECT s.*
+                        FROM logstreams s, devices d
+                        WHERE s.sender_mac = d.mac AND d.name IN %s;""" % sender_names
+            return self.execute(sql)
+
     def format_logs_query(self, projections, ordering=None, \
-                    senders=None, history=(None,None), **kwargs):
+                    senders=None, history=(None,None), stream_ids=None, **kwargs):
         args = []
         constraints = [ 's.sender_mac = d.mac', 'l.stream_id = s.id' ]
+        if stream_ids:
+            stream_ids_sql = '''(%s)''' % ",".join(
+                str(stream_id) for stream_id in stream_ids)
+            constraints.append('s.id IN %s' % stream_ids_sql)
         if senders:
-            constraints.append('''d.name IN ('%s') ''' % "','".join(senders))
+            sender_names = '''('%s')''' % "','".join(senders)
+            constraints.append('d.name IN %s' % sender_names)
         start, end = history
         if start:
             constraints.append('l.timestamp > %s')

commit 4604be484deca446fcd9e414d43f40f1adc47c87

Fix missing import
Fixes: 14c378a9e14b ("walt log show: fix count_logs() when specifying streams")

server/walt/server/threads/main/server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import re
+import pickle
 from walt.common.constants import WALT_SERVER_TCP_PORT
 from walt.common.devices.registry import get_device_cls_from_vci_and_mac
 from walt.common.tcp import TCPServer