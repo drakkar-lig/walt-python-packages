23e844888557f8c5932e951f5ebdf3e1ee3b48e3
Etienne Dublé <etienne.duble@imag.fr>
2017-12-18 14:57:47 +0100

    node shell: increase connect timeout to 10s.

--- server/walt/server/const.py
+++ server/walt/server/const.py
@@ -7,7 +7,7 @@ SNMP_TIMEOUT                = 3
 DOCKER_HUB_GET_TAGS_URL     = 'https://registry.hub.docker.com/v1/repositories/%(image_name)s/tags'
 WALT_DBNAME                 = "walt"
 WALT_DBUSER                 = "root"
-SSH_COMMAND                 = "ssh -o StrictHostKeyChecking=no -o ConnectTimeout=5"
+SSH_COMMAND                 = "ssh -o StrictHostKeyChecking=no -o ConnectTimeout=10"
 UI_FIFO_PATH                = '/var/lib/walt/ui.fifo'
 UI_RESPONSE_FIFO_PATH       = '/var/lib/walt/ui-response.fifo'
 WALT_NODE_NET_SERVICE_PORT  = 12346
