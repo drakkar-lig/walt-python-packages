9897f697a12cda51186e24f2779727b1dd0909ed
Etienne Dublé <etienne.duble@imag.fr>
2019-06-05 13:35:25 +0200

    */setup.py: ensure python3 is used.

--- virtual/setup.py
+++ virtual/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 import sys
 
-if not sys.version_info[0] == 2:
-    sys.exit("Sorry, Python 3 is not supported (yet)")
+if sys.version_info[0] == 2:
+    sys.exit("Sorry, Python 2 is no longer supported.")
 
 # make sure we are executing the new info.py
 execfile('walt/virtual/info.py')
