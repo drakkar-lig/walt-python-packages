d512fa4d87c1c430bb312dc0b5214ea76077665e
Etienne Dublé <etienne.duble@imag.fr>
2019-06-12 16:27:45 +0200

    py3 -- fix netgear prefix recognition.

--- common/walt/common/devices/switches/netgear.py
+++ common/walt/common/devices/switches/netgear.py
@@ -1,14 +1,11 @@
-NETGEAR_MAC_PREFIX = "6c:b0:ce"
 NETGEAR_MAC_PREFIXES = [ "6c:b0:ce", "28:c6:8e", "44:94:fc" ]
 
 class NetgearSwitch(object):
-    MAC_PREFIX = NETGEAR_MAC_PREFIX
     MODEL_NAME = "netgear"
     WALT_TYPE  = "switch"
     @staticmethod
     def is_such_a_device(vci, mac):
-        #return mac.startswith(NETGEAR_MAC_PREFIX)
-        return (len(filter(mac.startswith,NETGEAR_MAC_PREFIXES+[''])[0]) != 0)
+        return max(mac.startswith(prefix) for prefix in NETGEAR_MAC_PREFIXES)
 
 def get_device_classes():
     return (NetgearSwitch,)
