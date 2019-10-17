07ef101c51bc1d2f1723a42c1fc2ab5b377ef420
Etienne Dublé <etienne.duble@imag.fr>
2019-10-17 13:31:18 +0000

    walt-virtual-node: add option --attach-usb

--- virtual/walt/virtual/node.py
+++ virtual/walt/virtual/node.py
@@ -5,6 +5,7 @@ from walt.common.apilink import ServerAPILink
 from walt.virtual.fakeipxe import ipxe_boot
 from walt.virtual.udhcpc import udhcpc_fake_netboot
 from plumbum import cli
+from pathlib import Path
 
 OS_ENCODING = sys.stdout.encoding
 HOST_CPU = platform.machine()
@@ -12,15 +13,13 @@ HOST_CPU = platform.machine()
 # the following values have been selected from output of
 # qemu-system-<host-cpu> -machine help
 if HOST_CPU == 'x86_64':
-    QEMU_MACHINE    = 'pc'
-    QEMU_PRODUCT    = 'Standard PC (i440FX + PIIX, 1996)'
-    QEMU_CPU        = 'host'
-    QEMU_NET_DRIVER = 'virtio-net-pci'
+    QEMU_MACHINE_DEF = '-machine pc -cpu host'
+    QEMU_PRODUCT     = 'Standard PC (i440FX + PIIX, 1996)'
+    QEMU_NET_DRIVER  = 'virtio-net-pci'
 elif HOST_CPU == 'aarch64':
-    QEMU_MACHINE    = 'virt-3.1'
-    QEMU_PRODUCT    = 'QEMU 3.1 ARM Virtual Machine'
-    QEMU_CPU        = 'host,aarch64=off'   # we use 32-bits mode
-    QEMU_NET_DRIVER = 'virtio-net-device'
+    QEMU_MACHINE_DEF = '-machine virt-3.1 -machine highmem=off -cpu host,aarch64=off' # we use 32-bits mode
+    QEMU_PRODUCT     = 'QEMU 3.1 ARM Virtual Machine'
+    QEMU_NET_DRIVER  = 'virtio-net-device'
 else:
     raise Exception('Unknown host CPU: ' + HOST_CPU)
 
@@ -29,9 +28,8 @@ QEMU_PROG = "qemu-system-" + HOST_CPU
 QEMU_RAM = 512
 QEMU_CORES = 4
 QEMU_ARGS = QEMU_PROG + " \
-                -enable-kvm \
-                -machine " + QEMU_MACHINE + "\
-                -cpu " + QEMU_CPU + "\
+                -enable-kvm " + \
+                QEMU_MACHINE_DEF + "\
                 -m " + str(QEMU_RAM) + "\
                 -name %(name)s \
                 -smp " + str(QEMU_CORES) + "\
@@ -41,18 +39,29 @@ QEMU_ARGS = QEMU_PROG + " \
                 -serial mon:stdio \
                 -no-reboot"
 
-"""
-qemu-system-aarch64 -enable-kvm -m 512 -nographic -machine virt -cpu host,aarch64=off -kernel kernel -append "$bootargs" -netdev bridge,br=walt-net,id=mynet -device virtio-net-device,mac=${ethaddr},netdev=mynet -smp 4 -no-reboot -device nec-usb-xhci,id=xhci \
-           -device usb-host,hostbus=1,hostport=1.2 \
-           -device usb-host,hostbus=1,hostport=1.3 \
-           -device usb-host,hostbus=1,hostport=1.4 \
-           -device usb-host,hostbus=1,hostport=1.5
-"""
-
+def get_qemu_usb_args():
+    model_file = Path('/proc/device-tree/model')
+    if not model_file.exists():
+        raise Exception('Do not know how to map USB ports on this host device (no /proc/device-tree/model file).')
+    model = model_file.read_text()
+    if model.startswith('Raspberry Pi 3 Model B Plus'):
+        usb_ports = (
+            ('1', '1.1.2'),  # upper left
+            ('1', '1.1.3'),  # lower left
+            ('1', '1.3'),    # upper right
+            ('1', '1.2')     # lower right
+        )
+    elif model.startswith('Raspberry Pi 3 Model B'):
+        raise Exception('TODO')
+    else:
+        raise Exception('Do not know how to map USB ports on this host device (%s).' % model)
+    usb_args = '-device nec-usb-xhci,id=xhci '
+    usb_args += ' '.join(('-device usb-host,hostbus=%s,hostport=%s' % t) for t in usb_ports)
+    return usb_args
 
 USAGE = """\
-Usage: %(prog)s --mac <node_mac> --ip <node_ip> --model <node_model> --hostname <node_name> --server-ip <server_ip>
-       %(prog)s --net-conf-udhcpc --mac <node_mac> --model <node_model>\
+Usage: %(prog)s [--attach-usb] --mac <node_mac> --ip <node_ip> --model <node_model> --hostname <node_name> --server-ip <server_ip>
+       %(prog)s [--attach-usb] --net-conf-udhcpc --mac <node_mac> --model <node_model>\
 """
 
 def get_env_start(info):
@@ -68,10 +77,13 @@ def get_env_start(info):
         env['fake-network-setup'] = udhcpc_fake_netboot
     else:
         env['fake-network-setup'] = api_fake_netboot
+    qemu_args = QEMU_ARGS
+    if info._attach_usb:
+        qemu_args += ' ' + get_qemu_usb_args()
     env.update({
         "manufacturer": MANUFACTURER,
         "product": QEMU_PRODUCT,
-        "qemu-args": ' '.join(QEMU_ARGS.split())
+        "qemu-args": ' '.join(qemu_args.split())
     })
     return env
 
@@ -117,11 +129,17 @@ def node_loop(info):
         time.sleep(120)
 
 class WalTVirtualNode(cli.Application):
-    _udhcpc = False     # default
+    _udhcpc = False         # default
+    _attach_usb = False     # default
 
     """run a virtual node"""
     def main(self):
         node_loop(self)
+
+    @cli.switch("--attach-usb")
+    def set_attach_usb(self):
+        """attach host USB ports to virtual node"""
+        self._attach_usb = True
 
     @cli.switch("--mac", str)
     def mac(self, mac_address):
