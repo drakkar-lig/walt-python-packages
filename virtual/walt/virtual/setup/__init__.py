25bd6b7062bcd27b191de6c1fe716b62ae2e6d21
Etienne Dublé <etienne.duble@imag.fr>
2019-10-29 13:58:11 +0000

    walt-virtual-setup: refactor, add option to install a vpn client

--- virtual/walt/virtual/setup/__init__.py
+++ virtual/walt/virtual/setup/__init__.py
@@ -3,18 +3,24 @@ import os, sys
 from walt.common.tools import failsafe_symlink
 from pkg_resources import resource_string
 from pathlib import Path
+from plumbum import cli
 
-SYSTEMD_SERVICE_FILES = [ "walt-vpn-server.service", "walt-vpn-server.socket" ]
+SYSTEMD_SERVER_SERVICE_FILES = [ "walt-vpn-server.service", "walt-vpn-server.socket" ]
 SYSTEMD_SERVICES_DIR = Path("/etc/systemd/system")
 
-def run():
+BUSYBOX_VPN_CLIENT_SERVICE_FILES = [ "S51waltvpnclient", "S52waltvirtualnode" ]
+BUSYBOX_SERVICES_DIR = Path("/etc/init.d")
+
+def setup_server(init_system):
+    if init_system != 'SYSTEMD':
+        sys.exit("Setting up a server with %s init system is not implemented. Exiting." % init_system)
     if os.geteuid() != 0:
         sys.exit("This script must be run as root. Exiting.")
-    for filename in SYSTEMD_SERVICE_FILES:
+    for filename in SYSTEMD_SERVER_SERVICE_FILES:
         service_file_content = resource_string(__name__, filename)
         service_file_path = SYSTEMD_SERVICES_DIR / filename
         if service_file_path.exists():
-            sys.exit('Virtual tools are already setup. Exiting.')
+            sys.exit('walt-virtual services are already setup. Exiting.')
         service_file_path.write_bytes(service_file_content)
         if filename.endswith('.socket'):
             # the following is the same as running 'systemctl enable <unit>.socket'
@@ -22,3 +28,42 @@ def run():
             failsafe_symlink(str(service_file_path),
                              str(SYSTEMD_SERVICES_DIR / "sockets.target.wants" / filename))
     print('Done.')
+
+def setup_vpn_client(init_system):
+    if init_system != 'BUSYBOX':
+        sys.exit("Setting up a vpn client with %s init system is not implemented. Exiting." % init_system)
+    for filename in BUSYBOX_VPN_CLIENT_SERVICE_FILES:
+        service_file_content = resource_string(__name__, filename)
+        service_file_path = BUSYBOX_SERVICES_DIR / filename
+        if service_file_path.exists():
+            sys.exit('walt-virtual services are already setup. Exiting.')
+        service_file_path.write_bytes(service_file_content)
+        service_file_path.chmod(0o755)  # make it executable
+    print('Done.')
+
+def setup(info):
+    if info._type == 'SERVER':
+        setup_server(info._init_system)
+    elif info._type == 'VPN_CLIENT':
+        setup_vpn_client(info._init_system)
+
+class WalTVirtualSetup(cli.Application):
+    _type = None
+    _init_system = None
+
+    def main(self):
+        """install walt-virtual software"""
+        setup(self)
+
+    @cli.switch("--type", cli.Set('SERVER', 'VPN_CLIENT', case_sensitive = False), mandatory = True)
+    def set_type(self, install_type):
+        """indicate which kind of setup is requested on this device"""
+        self._type = install_type.upper()
+
+    @cli.switch("--init-system", cli.Set('SYSTEMD', 'BUSYBOX', case_sensitive = False), mandatory = True)
+    def set_init_system(self, init_system):
+        """indicate the init system available on this device"""
+        self._init_system = init_system.upper()
+
+def run():
+    WalTVirtualSetup.run()
