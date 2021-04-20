9b393fe35ce0930df573b2e15c58b9b4b3064f0f
Etienne Dublé <etienne.duble@imag.fr>
2021-04-20 16:17:41 +0200

    client-g5k setup.py: verify we are on a G5K frontend.

--- client-g5k/setup.py
+++ client-g5k/setup.py
@@ -1,8 +1,16 @@
-from setuptools import setup, find_packages
 import sys
 
 if sys.version_info[0] == 2:
     sys.exit("Sorry, Python 2 is no longer supported.")
+
+from shutil import which
+
+for cmd in ('oarsub', 'oarprint', 'kareboot3', 'kavlan'):
+    if which(cmd) is None:
+        sys.exit(("'%s' not found. " % cmd) + \
+                 "walt-client with 'g5k' extension must be installed on a G5K frontend.")
+
+from setuptools import setup, find_packages
 
 # make sure we are executing the new info.py
 exec(compile(open('walt/client/g5k/info.py').read(), 'walt/client/g5k/info.py', 'exec'))
