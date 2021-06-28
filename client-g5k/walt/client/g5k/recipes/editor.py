5ed2c6c181a784b8a6d01944053e046e6d5b2fd1
Etienne Dublé <etienne.duble@imag.fr>
2021-06-28 14:40:56 +0200

    client-g5k / recipe edit: let the user toggle start time estimation (or not).

--- client-g5k/walt/client/g5k/recipes/editor.py
+++ client-g5k/walt/client/g5k/recipes/editor.py
@@ -24,7 +24,7 @@ def human_readable_time(ts):
         return 'Tomorrow at ' + start.strftime("%X")
     return start.strftime("%c")
 
-def print_recipe_status(recipe_info):
+def print_recipe_status(context, recipe_info):
     s = 'Recipe status: '
     undefined = []
     if recipe_info['server']['site'] is None:
@@ -36,27 +36,32 @@ def print_recipe_status(recipe_info):
                 ' (tip: define ' + ', '.join(undefined) + ')')
         return False    # invalid
     print()
-    busy_message = 'Analysing... '
-    sys.stdout.write(busy_message)
-    sys.stdout.flush()
-    valid, res_info = analyse_reservation(recipe_info)
-    sys.stdout.write('\r' + ' ' * len(busy_message) + '\r')
-    sys.stdout.flush()
-    if valid:
-        startup = res_info['selected_slot']['start']
-        print(s + 'OK')
-        print('Estimated reservation start:', human_readable_time(startup))
+    if context['start_time_estimation']:
+        busy_message = 'Analysing... '
+        sys.stdout.write(busy_message)
+        sys.stdout.flush()
+        valid, res_info = analyse_reservation(recipe_info)
+        sys.stdout.write('\r' + ' ' * len(busy_message) + '\r')
+        sys.stdout.flush()
+        if valid:
+            startup = res_info['selected_slot']['start']
+            print(s + 'OK')
+            print('Estimated reservation start:', human_readable_time(startup))
+        else:
+            print(s + highlight('not available') + \
+                    ' (tip: ' + res_info['tip'] + ')')
     else:
-        print(s + highlight('not available') + \
-                ' (tip: ' + res_info['tip'] + ')')
+        valid = True
+        print(s + 'OK')
+        print('Estimated reservation start: <not-computed>')
     return valid
 
-def define_walt_server(recipe_info):
+def define_walt_server(context, recipe_info):
     site = choose('On which site should WalT server run?', \
                   get_g5k_sites_for_walt_server())
     recipe_info['server']['site'] = site
 
-def change_walt_nodes(recipe_info):
+def change_walt_nodes(context, recipe_info):
     site = choose('On which site should WalT nodes be defined?', get_g5k_sites())
     number = prompt('Define how many WalT nodes should run there', type=int,
                     validator = lambda x: x >= 0)
@@ -80,29 +85,34 @@ def validate_walltime(wt):
         print('Expected format is the one of oarsub command (e.g. 01:00:00 for one hour).')
     return valid
 
-def change_walltime(recipe_info):
+def change_walltime(context, recipe_info):
     print('G5K reservation walltime defines the lifetime of your temporary WalT platform.')
     recipe_info['walltime'] = prompt('Please enter it (oarsub format, e.g. 01:00:00)',
                     validator = validate_walltime)
 
-def change_schedule(recipe_info):
+def change_schedule(context, recipe_info):
     print('G5K reservation schedule defines the time when deployment will start.')
     options = { v:k for k, v in SCHEDULE_DESC.items() }
     recipe_info['schedule'] = choose('Please select the relevant option:', options)
 
+def toggle_start_time_estimation(context, recipe_info):
+    context['start_time_estimation'] = not context['start_time_estimation']
+
 def edit_recipe(recipe_info):
     with alternate_screen_buffer():
+        context = { "start_time_estimation": False }
         while True:
             clear_screen()
             print()
             print_recipe(recipe_info)
-            valid = print_recipe_status(recipe_info)
+            valid = print_recipe_status(context, recipe_info)
             print()
             options = {
                 'define WalT server site':          (True, define_walt_server),
                 'change WalT node counts':          (True, change_walt_nodes),
                 'change G5K reservation schedule':  (True, change_schedule),
                 'change G5K reservation walltime':  (True, change_walltime),
+                'toggle start time estimation':     (True, toggle_start_time_estimation),
                 'abort':                            (False, False),
             }
             if valid:
@@ -111,7 +121,7 @@ def edit_recipe(recipe_info):
             if selected[0] is True:
                 action = selected[1]
                 print()
-                action(recipe_info)
+                action(context, recipe_info)
             else:
                 return selected[1]
 
