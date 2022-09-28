63489a418c90d03f3d09a611cb8da4030beca7e1
Etienne Dublé <etienne.duble@imag.fr>
2022-09-28 11:18:03 +0200

    make test: fix tab completion after node network config was added

--- test/completion.sh
+++ test/completion.sh
@@ -31,7 +31,7 @@ test_tab_complete "walt image cp $image:/bin/gunz" "ip "
 test_tab_complete "walt image cp $image:/bin/gunzip $tmp_dir" "/"
 test_tab_complete "walt node config $node ram=4G netset" "up="
 test_tabtab_complete "walt node reboot my-nodes" "my-nodes   my-nodes,"
-test_tabtab_complete "walt node config $node " "cpu.cores=    disks=        kexec.allow=  netsetup=     ram="
+test_tabtab_complete "walt node config $node " "cpu.cores=    disks=        kexec.allow=  netsetup=     networks=     ram="
 EOF
 
     # cleanup
