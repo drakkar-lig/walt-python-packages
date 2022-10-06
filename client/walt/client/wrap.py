410f17766cdbba81bd18797aec00cad94098a0fe
Etienne Dublé <etienne.duble@imag.fr>
2022-10-06 14:56:13 +0200

    client: return 1 if main() function returns False, 0 if True

--- client/walt/client/wrap.py
+++ client/walt/client/wrap.py
@@ -7,7 +7,21 @@ def wrap_client_command(f):
     def wrapped(*args, **kwargs):
         try:
             timeout_init_handler()
-            f(*args, **kwargs)
+            # note: plumbum.cli applications end by calling sys.exit(<retcode>).
+            # Here, by convention, when main() function returns False it means "failure",
+            # but sys.exit(False) translates to a return code int(False) which leads to
+            # a call equivalent to sys.exit(0) (i.e. success).
+            # So we detect this case and replace False by 1.
+            # Similarly, we replace True by 0.
+            try:
+                f(*args, **kwargs)
+            except SystemExit as e:
+                if e.code is False:
+                    sys.exit(1)
+                elif e.code is True:
+                    sys.exit(0)
+                else:
+                    raise e
         except socket.error:
             hook = get_hook('failing_server_socket')
             if hook is not None:
