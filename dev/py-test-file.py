d3a1b02959b1fb7d27caf38c9c4c40dfe8c52364
Etienne Dublé <etienne.duble@imag.fr>
2023-03-21 15:33:07 +0100

    make test: add infrastructure for python tests.

---
+++ dev/py-test-file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import sys, atexit
+from pathlib import Path
+# find imports from the current working directory, not
+# from the directory of this source file
+sys.path[0] = str(Path().resolve())
+from includes.common import set_py_test_mode
+
+def main():
+    src_file = Path(sys.argv[1])
+    set_py_test_mode(*sys.argv[2:])
+    # copy file to make sure it has valid chars for a python module
+    test_module = Path() / '__py_test_module.py'
+    test_module.write_text(src_file.read_text())
+    atexit.register(lambda: test_module.unlink())
+    import __py_test_module
+
+if __name__ == '__main__':
+    main()
