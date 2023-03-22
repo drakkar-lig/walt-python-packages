ded1f9f5d58f8431539dcd00a083f7b64021f98f
Etienne Dublé <etienne.duble@imag.fr>
2023-03-22 13:56:03 +0100

    make test: add api tests

---
+++ test/api-root.py
@@ -0,0 +1,14 @@
+from includes.common import define_test
+
+API_ROOT_REPR = '''\
+< -- WALT API root --
+
+  read-only attributes:
+  - self.images: <API submodule for WALT images>
+  - self.nodes: <API submodule for WALT nodes>
+>'''
+
+@define_test('repr(api)')
+def api_root_repr():
+    from walt.client import api
+    assert repr(api) == API_ROOT_REPR
