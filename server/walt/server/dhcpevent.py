ca8d1c27be546b3f91ea4f669c63c97337ca197b
Etienne Dublé <etienne.duble@imag.fr>
2023-03-29 18:23:14 +0200

    server dhcp event: fix unknown device -> node transition
    Users sometimes connect new nodes to the platform without
    a proper bootloader configured. This is useful to check
    that the hardware is working for instance, and/or when
    developing the support of a new node model.
    With the current code, walt-dhcp-event was no longer
    called after the first DHCP request (because the device
    already had a host entry at subsequent requests), thus
    the main daemon could not detect that the dhcp UCI or VCI
    option was now properly set to 'walt.node.<something>'.
    
    This commit changes the DHCP configuration template
    in order to start walt-dhcp-event whenever the device type
    is unknown.
    
    However, the code of walt-dhcp-event was also modified to
    filter requests more finely and avoid bothering the walt
    server main daemon with useless requests.

--- server/walt/server/dhcpevent.py
+++ server/walt/server/dhcpevent.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python
 import sys
-from walt.common.apilink import ServerAPILink
 
-def register_device(vci, uci, ip, mac, client_name=None):
+def dhcp_commit_event(vci, uci, ip, mac, client_name,
+                      mac_is_known, dev_type):
+    mac_is_known = int(mac_is_known)
+    # we want to:
+    # 1. detect new devices (i.e. new mac addresses)
+    # 2. allow a device already seen previously but of
+    #    unknown type to resend a new notification to
+    #    the server to update its type (i.e. become a node)
+    # Case 2 is frequent when users first connect a node
+    # to the walt network without a proper bootloader set up,
+    # then install the bootloader and reconnect the node.
+    if mac_is_known:
+        if dev_type != 'unknown':
+            return  # nothing to do, we already know everything
+        if not uci.startswith('walt.node.') and \
+           not vci.startswith('walt.node.'):
+            return  # this device of unknown type will remain
+                    # of unknown type, so do not bother the main
+                    # daemon with a useless request
     name = None
     if client_name is not None and client_name != '':
         mac_suffix = ''.join(mac.split(':'))[-6:]
         name = f'{client_name}-{mac_suffix}'.lower()
+    from walt.common.apilink import ServerAPILink
     with ServerAPILink('localhost', 'SSAPI') as server:
         server.register_device(vci, uci, ip, mac, name)
 
@@ -14,7 +32,7 @@ def run():
     if sys.argv[1] != 'commit':
         print('unexpected dhcp event:', sys.argv[1])
         return
-    register_device(*sys.argv[2:])
+    dhcp_commit_event(*sys.argv[2:])
 
 if __name__ == "__main__":
     run()
