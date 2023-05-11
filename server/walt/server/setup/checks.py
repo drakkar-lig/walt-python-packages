b5330ee1984e504120f8c72d04753c427b3b9ed1
Etienne Dublé <etienne.duble@imag.fr>
2023-05-11 10:19:40 +0200

    walt-server-setup: remove the need for gpg

--- server/walt/server/setup/checks.py
+++ server/walt/server/setup/checks.py
@@ -1,4 +1,4 @@
-import sys, shutil
+import sys
 
 # check that we have the python apt module
 # (also available via pip, but does not install well on debian,
@@ -9,9 +9,3 @@ except:
     print('"apt" module not found. Run the following:')
     print('$ apt update && apt install -y python3-apt')
     sys.exit(1)
-
-if shutil.which('gpg') is None:
-    print('"gpg" command not found. Run the following:')
-    print('$ apt update && apt install -y gpg')
-    sys.exit(1)
-
