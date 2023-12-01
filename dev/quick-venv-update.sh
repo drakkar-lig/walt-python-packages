2b51d997fe6fa68eed631b58229686168e1690dd
Etienne Dublé <etienne.duble@imag.fr>
2023-12-01 11:12:41 +0100

    dev: make quick-venv-update
    Report file modifications in <python-sub-package>/walt/*
    to the virtual env (.venv/lib/python*/dist-packages/...),
    much faster than using 'make install'.
    Can save time when working on code affecting the server.

---
+++ dev/quick-venv-update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+git_root="$(git rev-parse --show-toplevel)"
+venv_root=$(python3 -c 'import sys; print(sys.prefix)')
+venv_packages="$(ls -d "${venv_root}/lib/python"*"/site-packages")"
+cd "$git_root"
+{
+    git ls-files --others --exclude-standard
+    git ls-files --modified
+} | grep "\<[a-z][a-z]*/walt/" | \
+    grep -v "\.swp$" | \
+    grep -v ":w$" | \
+    grep -v ":$" | \
+while read f
+do
+	mod_path=$(echo "$f" | sed -e 's/^[a-z]*\///')
+	venv_f="${venv_packages}/$mod_path"
+    if diff -q "$f" "$venv_f" >/dev/null 2>&1
+    then
+        # already modified / added in venv
+        continue
+    fi
+    if [ -e "$venv_f" ]
+    then
+        mode="updated"
+    else
+        mode="added"
+    fi
+    mkdir -p "$(dirname "$venv_f")"
+    cp "$f" "$venv_f"
+    echo $mode $mod_path in venv.
+done
