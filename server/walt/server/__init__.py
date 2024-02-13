6b1710d567ab16a476c35920369b65d607c25510
Etienne Dublé <etienne.duble@imag.fr>
2024-02-13 13:49:15 +0100

    server: lazy module import in __init__.py (for faster walt-dhcp-event)

--- server/walt/server/__init__.py
+++ server/walt/server/__init__.py
@@ -1,12 +1,11 @@
 import sys
 
-from walt.server.config import check_conf, get_conf
-
 
 def __getattr__(name):
     # Lazy-loading of the configuration: at setup time, this file
     # does not exist yet
     if name == "conf":
+        from walt.server.config import check_conf, get_conf
         check_conf()
         conf = get_conf()
         setattr(sys.modules[__name__], name, conf)
