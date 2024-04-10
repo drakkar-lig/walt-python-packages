b81f9fc1d8e223b8314bcd35b6f28fc79735e777
Etienne Dublé <etienne.duble@imag.fr>
2024-04-10 16:59:53 +0200

    server: optimize tftp update procedure

--- server/walt/server/processes/main/network/tftp.py
+++ server/walt/server/processes/main/network/tftp.py
@@ -1,4 +1,6 @@
 import os
+import numpy as np
+import pickle
 import shutil
 import tarfile
 from pathlib import Path
@@ -12,9 +14,47 @@ PXE_PATH = TFTP_ROOT + "pxe/"
 NODES_PATH = TFTP_ROOT + "nodes/"
 TFTP_STATIC_DIR = Path(TFTP_ROOT + "tftp-static")
 NODE_PROBING_PATH = Path(NODES_PATH) / 'probing'
+TFTP_STATUS_PATH = Path(NODES_PATH) / "status.pickle"
+TFTP_STATUS = None
+
+
+def save_tftp_status():
+    TFTP_STATUS_PATH.parent.mkdir(parents=True, exist_ok=True)
+    TFTP_STATUS_PATH.write_bytes(pickle.dumps(TFTP_STATUS))
+
+
+def is_real_dir(path):
+    return not path.is_symlink() and path.is_dir()
+
+
+def revert_to_empty_status():
+    global TFTP_STATUS
+    print("tftp: invalid or missing status file, resetting.")
+    # revert to an empty status and remove the previous content,
+    # except persist_dir, networks, disks if they exist
+    if Path(NODES_PATH).exists():
+        for node_entry in list(Path(NODES_PATH).iterdir()):
+            if is_real_dir(node_entry):
+                # if <mac>/persist is a directory, rename it to 'persist_dir'
+                # (compatibility with older walt code)
+                persist_entry = (node_entry / "persist")
+                if persist_entry.is_dir():
+                    persist_entry.rename("persist_dir")
+                for node_dir_entry in list(node_entry.iterdir()):
+                    if node_dir_entry.name not in (
+                            "persist_dir", "networks", "disks"):
+                        if is_real_dir(node_dir_entry):
+                            shutil.rmtree(node_dir_entry)
+                        else:
+                            node_dir_entry.unlink()
+            else:
+                node_entry.unlink()
+    TFTP_STATUS = set()
+    save_tftp_status()
 
 
 def prepare():
+    global TFTP_STATUS
     if not Path(PXE_PATH).exists():
         failsafe_makedirs(PXE_PATH)
         orig_path = resource_filename(__name__, "walt-x86-undionly.kpxe")
@@ -23,6 +63,10 @@ def prepare():
         archive_path = resource_filename(__name__, "tftp-static.tar.gz")
         with tarfile.open(archive_path) as tar:
             tar.extractall(str(TFTP_STATIC_DIR.parent))
+    if TFTP_STATUS_PATH.exists():
+        TFTP_STATUS = pickle.loads(TFTP_STATUS_PATH.read_bytes())
+    else:
+        revert_to_empty_status()
     if not NODE_PROBING_PATH.exists():
         NODE_PROBING_PATH.mkdir(parents=True)
         failsafe_symlink(
@@ -36,20 +80,12 @@ def prepare():
         shutil.rmtree(str(tftp_standby))
 
 
-def persist_symlink_path(node_mac):
-    return Path(NODES_PATH) / node_mac / "persist"
-
-
-def persist_dir_path(node_mac):
-    return Path(NODES_PATH) / node_mac / "persist_dir"
-
-
 # Note: NFSv4 needs to be able to read symlinks, thus
 # the whole /var/lib/walt/nodes directory is a read-only share.
 #
 # If a node has its config option "mount.persist" set to false,
 # removing /var/lib/walt/nodes/<mac>/persist from the exports file
-# is not enough: the client mount with still be accepted, as read-only,
+# is not enough: the client mount will still be accepted, as read-only,
 # because of the previous statement.
 #
 # That's why we actually manage a symlink at
@@ -57,80 +93,66 @@ def persist_dir_path(node_mac):
 # /var/lib/walt/nodes/<mac>/persist_dir in the usual situation
 # (mount.persist=true) and a missing path otherwise (mount.persist=false),
 # in order to make the mount fail on client side.
-def update_persist_files(node):
-    p_lnk = persist_symlink_path(node.mac)
-    p_dir = persist_dir_path(node.mac)
-    if node.conf.get("mount.persist", True):
-        p_lnk_target = "persist_dir"
-    else:
-        p_lnk_target = "forbidden_dir"
-    if p_lnk.is_symlink():
-        if str(p_lnk.readlink()) == p_lnk_target:
-            return  # already ok
-        else:
-            p_lnk.unlink()  # remove wrong symlink and continue
-    # if p_lnk is a directory, rename it to 'persist_dir'
-    # (compatibility with older walt code)
-    if p_lnk.is_dir():
-        p_lnk.rename(p_dir)
-    # ensure persist_dir exists
-    p_dir.mkdir(parents=True, exist_ok=True)
-    # create the correct symlink
-    p_lnk.symlink_to(p_lnk_target)
+#
+# each node has a directory entry with:
+# - a link called "fs" to the image filesystem root
+# - a link called "tftp" to a directory of boot files stored per-model in the image
+# - a directory called "persist" which is mounted at /persist on the node
+# The name of this dir is the mac address of the node,
+# written <hh>:<hh>:<hh>:<hh>:<hh>:<hh>.
+# For compatibility with different network bootloaders
+# we also provide 3 links to this directory:
+# - mac address written <hh>-<hh>-<hh>-<hh>-<hh>-<hh>
+# - ipv4 address (dotted quad notation)
+# - walt node name
+#
+# about case cleanup=True:
+# walt-server-daemon is going down, which will cause nodes to reboot.
+# Some node models may hang forever in the boot procedure if they cannot
+# download appropriate boot files using TFTP. This is the case for rpi 3b+
+# and later boards, whose firmware is able to boot over the network without
+# a SD card: if the firmware is not able to download the TFTP files, it
+# will hang. In this case, we replace the target of the 'tftp'
+# symlink, which normally targets '[image-root]:/boot/<model>', by
+# '/var/lib/walt/tftp-static', where appropriate boot files can
+# be found. These boot files will cause the node to continuously reboot
+# until walt-server-daemon is back.
 
-
-def update(db, images):
-    subnet = get_walt_subnet()
-    free_ips = set(str(ip) for ip in subnet.hosts())
-    server_ip = get_server_ip()
-    free_ips.discard(server_ip)
-    # create dir if it does not exist yet
-    failsafe_makedirs(NODES_PATH)
-    # list existing entries, in case some of them are obsolete
-    invalid_entries = set(f for f in os.listdir(NODES_PATH))
-    # each node has a directory entry with:
-    # - a link called "fs" to the image filesystem root
-    # - a link called "tftp" to a directory of boot files stored per-model in the image
-    # - a directory called "persist" which is mounted at /persist on the node
-    # The name of this dir is the mac address of the node,
-    # written <hh>:<hh>:<hh>:<hh>:<hh>:<hh>.
-    # For compatibility with different network bootloaders
-    # we also provide 3 links to this directory:
-    # - mac address written <hh>-<hh>-<hh>-<hh>-<hh>-<hh>
-    # - ipv4 address (dotted quad notation)
-    # - walt node name
-    for db_node in db.execute("""
-                SELECT d.mac, d.ip, d.name, d.conf, n.model, n.image
+def update(db, images, cleanup=False):
+    global TFTP_STATUS
+    db_nodes = db.execute("""
+                SELECT d.mac, d.ip, d.name, d.conf, n.model, n.image,
+                    REPLACE(d.mac, ':', '-') as mac_dash,
+                    COALESCE((d.conf->'mount.persist')::bool, true) as persist
                 FROM devices d, nodes n
-                WHERE d.mac = n.mac"""):
-        mac = db_node.mac
-        model = db_node.model
-        mac_dash = mac.replace(":", "-")
-        free_ips.discard(db_node.ip)
-        failsafe_makedirs(NODES_PATH + mac)
-        update_persist_files(db_node)
-        for ln_name in (mac_dash, db_node.ip, db_node.name):
-            failsafe_symlink(
-                NODES_PATH + mac, NODES_PATH + ln_name, force_relative=True
-            )
-            # this entry is valid
-            invalid_entries.discard(ln_name)
-        invalid_entries.discard(mac)
-        if db_node.image is None:
-            continue
-        image = images[db_node.image]
-        image_path = image.mount_path
-        if image_path is None:
-            # when overwritting a mounted image, we umount it even if it is in use,
-            # and we get here.
-            continue
-        failsafe_symlink(image_path, NODES_PATH + mac + "/fs", force_relative=True)
-        # link to boot files stored inside the image
-        failsafe_symlink(
-            image_path + "/boot/" + model,
-            NODES_PATH + mac + "/tftp",
-            force_relative=True,
-        )
+                WHERE d.mac = n.mac""")
+    # -- declare dirs
+    mac_dirs = "DIR " + db_nodes.mac
+    # -- declare ip, name and mac-dash symlinks
+    mac_dash_symlinks = "SYMLINK " + db_nodes.mac + " " + db_nodes.mac_dash
+    ip_symlinks = "SYMLINK " + db_nodes.mac + " " + db_nodes.ip
+    name_symlinks = "SYMLINK " + db_nodes.mac + " " + db_nodes.name
+    # -- declare fs and tftp symlinks
+    # if this function was called as part of the cleanup procedure,
+    # the symlink <mac>/tftp is redirected to TFTP_STATIC_DIR
+    # (see comment about case cleanup=True above)
+    metadata = images.registry.get_multiple_metadata(db_nodes.image)
+    image_ids = np.fromiter((m["image_id"] for m in metadata), dtype=object)
+    fs_symlinks = (
+        "SYMLINK ../../images/" + image_ids + "/fs " + db_nodes.mac + "/fs")
+    if cleanup:
+        tftp_symlinks = ("SYMLINK ../../tftp-static " + db_nodes.mac + "/tftp")
+    else:
+        tftp_symlinks = (
+            "SYMLINK ../../images/" + image_ids + "/fs/boot/" + db_nodes.model +
+            " " + db_nodes.mac + "/tftp")
+    # -- declare persist symlinks
+    mask_persist = db_nodes.persist.astype(bool)
+    persist_ok_symlinks = (
+        "SYMLINK persist_dir " + db_nodes.mac[mask_persist] + "/persist")
+    persist_ko_symlinks = (
+        "SYMLINK forbidden_dir " + db_nodes.mac[~mask_persist] + "/persist")
+    # -- declare symlinks to node-probing dir for unallocated ips
     # Raspberry pi 3b+ boards do not implement the whole DHCP handshake and
     # try to use the IP offered directly without requesting it. So in case the
     # device is new, the dhcp commit event is never called, walt-dhcp-event is
@@ -141,41 +163,67 @@ def update(db, images):
     # will allow to detect the rpi model and redo the DHCP handshake properly,
     # so the server is aware of it and can direct next requests to a default
     # image.
-    for free_ip in free_ips:
-        failsafe_symlink(
-            str(NODE_PROBING_PATH),
-            NODES_PATH + free_ip,
-            force_relative=True
-        )
-        invalid_entries.discard(free_ip)  # valid dir entry at <free_ip>
-    invalid_entries.discard("probing")    # valid dir entry at "probing"
-    # if there are still values in variable invalid_entries,
-    # we can remove the corresponding entry
-    for entry in invalid_entries:
-        entry = NODES_PATH + entry
-        if os.path.isdir(entry) and not os.path.islink(entry):
-            shutil.rmtree(entry)
-        else:
-            os.remove(entry)
-
-
-def cleanup(db):
-    # walt-server-daemon is going down, which will cause nodes to reboot.
-    # Some node models may hang forever in the boot procedure if they cannot
-    # download appropriate boot files using TFTP. This is the case for rpi 3b+
-    # and later boards, whose firmware is able to boot over the network without
-    # a SD card: if the firmware is not able to download the TFTP files, it
-    # will hang. In this cleanup procedure, we replace the target of the 'tftp'
-    # symlink, which normally targets '[image-root]:/boot/<model>', by
-    # '/var/lib/walt/tftp-static', where appropriate boot files can
-    # be found. These boot files will cause the node to continuously reboot
-    # until walt-server-daemon is back.
-    if not TFTP_STATIC_DIR.exists():
-        # There was an issue in startup code before tftp.prepare() could be called
+    subnet = get_walt_subnet()
+    free_ips = set(str(ip) for ip in subnet.hosts())
+    server_ip = get_server_ip()
+    free_ips.discard(server_ip)
+    free_ips -= set(db_nodes.ip)
+    free_ips = np.array(list(free_ips), dtype=object)
+    free_ip_symlinks = f"SYMLINK probing " + free_ips
+    # -- compile the new status
+    status = set(np.concatenate((
+        mac_dirs, mac_dash_symlinks, ip_symlinks, name_symlinks,
+        fs_symlinks, tftp_symlinks, persist_ok_symlinks, persist_ko_symlinks,
+        free_ip_symlinks), dtype=object))
+    if status == TFTP_STATUS:
+        # nothing changed
         return
-    for db_node in db.select("nodes"):
-        mac = db_node.mac
-        model = db_node.model
-        failsafe_symlink(
-            str(TFTP_STATIC_DIR), NODES_PATH + mac + "/tftp", force_relative=True
-        )
+    while True:
+        valid_status = True
+        # -- remove entries of old status no longer present
+        # note: we sort to ensure SYMLINK directives are first
+        for directive in sorted(TFTP_STATUS - status, reverse=True):
+            args = directive.split()
+            if args[0] == "SYMLINK":
+                if not Path(NODES_PATH + args[2]).is_symlink():
+                    # the status file contains invalid information
+                    valid_status = False
+                    break
+                #print(f"tftp: remove {args[2]}")
+                Path(NODES_PATH + args[2]).unlink()
+            elif args[0] == "DIR":
+                if not is_real_dir(Path(NODES_PATH + args[1])):
+                    # the status file contains invalid information
+                    valid_status = False
+                    break
+                #print(f"tftp: remove {args[1]}")
+                shutil.rmtree(NODES_PATH + args[1])
+        if not valid_status:
+            revert_to_empty_status()
+            continue
+        # -- add new entries
+        # notes:
+        # * we sort to ensure DIR directives are first
+        # * some dir entries might already be present because
+        #   we kept persist_dir, networks, disks subdirs in prepare() above.
+        for directive in sorted(status - TFTP_STATUS):
+            args = directive.split()
+            if args[0] == "DIR":
+                #print(f"tftp: create {args[1]}")
+                mac_dir_path = Path(NODES_PATH + args[1])
+                mac_dir_path.mkdir(exist_ok=True)
+                (mac_dir_path / "persist_dir").mkdir(exist_ok=True)
+            elif args[0] == "SYMLINK":
+                if Path(NODES_PATH + args[2]).exists():
+                    # the status file contains invalid information
+                    valid_status = False
+                    break
+                #print(f"tftp: create {args[2]}")
+                Path(NODES_PATH + args[2]).symlink_to(args[1])
+        if not valid_status:
+            revert_to_empty_status()
+            continue
+        # -- save the new status
+        TFTP_STATUS = status
+        save_tftp_status()
+        return  # ok
