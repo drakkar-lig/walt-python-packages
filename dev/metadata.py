c1fd4ffb9752b7b2c2590088287f302b57acf91c
Etienne Dublé <etienne.duble@imag.fr>
2024-05-06 14:38:00 +0200

    server deps: upgrade snimpy but limit pysnmp-lextudio<6
    See https://github.com/vincentbernat/snimpy/issues/111

--- dev/metadata.py
+++ dev/metadata.py
@@ -76,7 +76,8 @@ PACKAGE_SPECIFIC_INFO = {
         requires=[
             "python-apt-binary",  # https://github.com/drakkar-lig/python-apt-binary
             "plumbum>=1.7.2",
-            "snimpy==1.0.0",      # https://github.com/vincentbernat/snimpy/issues/111
+            "snimpy>=1.0.2",
+            "pysnmp-lextudio<6",   # https://github.com/vincentbernat/snimpy/issues/111
             "ipaddress>=1.0.7",
             "requests>=2.21.0",
             "sdnotify>=0.3.0",
