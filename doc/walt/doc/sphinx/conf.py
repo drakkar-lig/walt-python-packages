6a63dce73e419b4481f55d2f97235acbbc151a29
Etienne Dublé <etienne.duble@imag.fr>
2024-09-30 15:20:34 +0200

    Fix web docs search function not working.
    Remove obsolete html_theme_path setting from doc/walt/doc/sphinx/conf.py.
    This was causing the search function to fail, as described here:
    https://github.com/readthedocs/sphinx_rtd_theme/issues/1434

--- doc/walt/doc/sphinx/conf.py
+++ doc/walt/doc/sphinx/conf.py
@@ -76,7 +76,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ["_static"]
 html_logo = "logo-walt.png"
 html_theme_options = {
