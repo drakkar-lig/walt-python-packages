022633a5d365a017d28a94d8a161096494cce27b
Etienne Dublé <etienne.duble@imag.fr>
2024-11-22 16:54:13 +0100

    walt-image-umount: loop if some processes prevent immediate umount
    When walt-server-daemon is interrupted, umounting images was sometimes
    failing because some processes such as in.tftpd, nbfsd or maybe nfsd where
    still serving the content of a file to a node.
    We now retry in a loop, and print the PIDs of processes involved if we
    can detect them by command `lsof`.

--- server/walt/server/mount/umount.py
+++ server/walt/server/mount/umount.py
@@ -1,8 +1,9 @@
 import functools
 import os
 import sys
+import time
 
-from walt.server.exttools import umount, buildah, podman
+from walt.server.exttools import umount, buildah, podman, lsof
 from walt.server.mount.tools import get_mount_path, mount_exists, serialized_mounts
 from walt.server.mount.tools import img_print as img_print_generic, long_image_id
 from walt.server.mount.tools import get_mount_container_name, get_mount_image_name
@@ -11,7 +12,20 @@ from walt.server.mount.tools import get_mount_container_name, get_mount_image_na
 def image_umount(image_id, mount_path):
     cont_name = get_mount_container_name(image_id)
     if mount_exists(mount_path):
-        umount(mount_path)
+        old_lsof = ""
+        while True:
+            try:
+                umount(mount_path, hide_stderr=True)
+                break   # ok, succeeded
+            except Exception:
+                new_lsof = lsof("-Q", "-Fp", mount_path)
+                if new_lsof != old_lsof and new_lsof != "":
+                    desc_pids = " ".join(l[1:] for l in new_lsof.splitlines())
+                    msg = f"held by process(es) with pid(s) {desc_pids}, will retry."
+                    img_print_generic(image_id, msg)
+                    old_lsof = new_lsof
+                # wait for the last processes to release this
+                time.sleep(0.05)
     buildah.umount(cont_name)
     buildah.rm(cont_name)
     image_name = get_mount_image_name(image_id)
