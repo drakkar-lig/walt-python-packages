6d6e0ad1d466963bb562fd95b1877e42f3df4071
Etienne Dublé <etienne.duble@imag.fr>
2025-12-04 11:48:49 +0100

    walt with no argument: print server and auto-completion status
    When typing `walt` with no arguments, a "status box" is now printed
    to display the status of the client installation:
    * the version (this was previously the only thing displayed there)
    * the server IP or hostname
    * the completion status (i.e., up-to-date or not).
    
    The completion status can be detected because we now export one new
    variable called _WALT_COMP_VERSION in the shell completion scripts
    generated by `walt advanced dump-bash-autocomplete` and
    `walt advanced dump-zsh-autocomplete`.
    See #135 for the original idea.
    
    Two other variables are also added, for possible future use:
    * `_WALT_COMP_TYPE`, with value `bash` or `zsh`
    * `_WALT_COMP_BASH_SOURCE` or `_WALT_COMP_ZSH_SOURCE`, giving the
      source path of the completion script.
    
    The logo printing code was updated because we now use a few escape
    codes for displaying this status box, so the length of some of the
    text lines is no longer the number of bytes.
    
    The documentation about shell completion was updated.

--- test/online-syntax-help.sh
+++ test/online-syntax-help.sh
@@ -1,22 +1,38 @@
 
 # note: to generate a file syntax/<cmd>.out use
-# LC_ALL=C <cmd> --help | sed -e "s/:{.*}//" > syntax/<cmd>.out
+# LC_ALL=C <cmd> --help > syntax/<cmd>.out
 # for instance:
-# LC_ALL=C walt advanced --help | sed -e "s/:{.*}//" > syntax/walt-advanced.out
+# LC_ALL=C walt advanced --help > syntax/walt-advanced.out
+
+# The grep commands allow to ignore volatile values of the "status box"
+# printed when typing `walt` with no arguments.
+# The sed expression is used to discard the list of values of a cli.Set,
+# because the order is not constant.
+# Command tr allows to ignore differences in spacing (may occur with a
+# different terminal size).
+filter_help_text() {
+    grep -v "Server:" |
+        grep -v "Version:" |
+        grep -v "Completion:" |
+        grep -v "^(\*)" |
+        sed -e "s/:{.*}//" |
+        tr -d '[:space:]' || true
+}
 
 check_expected_syntax() {
     out_filename="$1"
     # cmd is deduced from file name, by removing ".out" prefix,
     # and replacing at most 2 dashes per a space (walt <category> <command>)
-    cmd=$(echo "$out_filename" | sed -e "s/\.out$//" | sed -e 's/-/ /' | sed -e 's/-/ /')
-    expected="$(cat $TESTS_DIR/syntax/$out_filename | grep -v "Version:" | tr -d '[:space:]')"
-    # the sed expression is used to discard the list of values of a cli.Set,
-    # because the order is not constant
-    # tr allows to ignore differences in spacing (may occur with a different terminal size)
-    result="$(LC_ALL=C $cmd --help | grep -v "Version:" | sed -e "s/:{.*}//" | tr -d '[:space:]' || true)"
+    cmd=$(echo "$out_filename" |
+          sed -e "s/\.out$//" |
+          sed -e 's/-/ /' |
+          sed -e 's/-/ /')
+    expected="$(filter_help_text < $TESTS_DIR/syntax/$out_filename)"
+    result="$(LC_ALL=C $cmd --help | filter_help_text)"
     if [ "$result" != "$expected" ]
     then
-        echo "$out_filename: '$cmd' online syntax help differs from expected!"
+        echo "$out_filename: '$cmd' online syntax help differs" \
+             "from expected!"
         return 1    # fail
     fi
 }
