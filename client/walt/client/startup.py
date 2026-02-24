3ed414a310e6592f438bce5d414da9fe04e268b8
Etienne Dublé <etienne.duble@imag.fr>
2026-02-24 17:06:12 +0100

    client: detect Windows without WSL and exit early.

--- client/walt/client/startup.py
+++ client/walt/client/startup.py
@@ -1,3 +1,13 @@
+import platform
+import sys
+
+help_url = "https://walt.readthedocs.io/en/latest/client-install.html"
+if platform.system() == "Windows":
+    print("Sorry walt client software cannot run directly on Windows.")
+    print("Use WSL (Windows Subsystem for Linux) instead.")
+    print(f"See: {help_url}")
+    sys.exit(1)
+
 from walt.client.apiobject.root import APIRoot
 from walt.client.timeout import timeout_init_handler
 from walt.common.version import __version__
