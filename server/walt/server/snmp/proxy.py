88202691eb73890a4c32dec7cb76ccb8a381eebe
Etienne Dublé <etienne.duble@imag.fr>
2026-03-24 12:13:35 +0100

    server: optimize snmp variants handling
    1. Implement a "ping", i.e., query sysDescr OID to check
       if the remote device is responding
    2. If the device is responding and the LLDP or PoE variant
       is not known yet, then probe the variant, and if no
       working variant was found, save this result in the
       disk cache (variant=None), to avoid having to probe
       again next time.
    
    For switches which provide only BRIDGE data and not LLDP,
    the previous code was restarting the probing of the LLDP
    variant each time, and failed after a time out each time.
    Now, this is done only once, and since the ping succeeded,
    we can consider this LLDP variant probing did not fail
    because of a communication issue: it failed just because
    the device does not handle it. So we can keep this knowledge
    in cache.

--- server/walt/server/snmp/proxy.py
+++ server/walt/server/snmp/proxy.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
+import snimpy
 from snimpy.manager import Manager
 from walt.server import const
 from walt.server.snmp.bridge import BridgeProxy
 from walt.server.snmp.lldp import LLDPProxy
 from walt.server.snmp.poe import PoEProxy
+from walt.server.snmp.mibs import (
+    get_loaded_mibs,
+    load_mib,
+)
 
 SNMP_OPTS = {"retries": 2, "timeout": const.SNMP_TIMEOUT, "cache": True}
 
@@ -21,3 +26,12 @@ class Proxy(object):
             self.lldp = LLDPProxy(self.snmp, host)
         if bridge:
             self.bridge = BridgeProxy(self.snmp, host)
+    def ping(self):
+        # check an SNMP attribute which is almost always implemented
+        if b"SNMPv2-MIB" not in get_loaded_mibs():
+            load_mib(b"SNMPv2-MIB")
+        try:
+            self.snmp.sysDescr
+            return True
+        except snimpy.snmp.SNMPException:
+            return False
