469af5ba1ef1b679fc6f5ef088b4e157513eebdc
Kalvin-Giang Pichon <kalvin-giang.pichon@etu.univ-grenoble-alpes.fr>
2026-06-05 10:42:10 +0200

    Improve dev/quick-venv-update.sh
    * Also detect file modifications in the last 10 commits
    * Also detect file modifications in <python-package>/sh/
    * Handle file deletions

--- dev/quick-venv-update.sh
+++ dev/quick-venv-update.sh
@@ -7,29 +7,58 @@ cd "$git_root"
 [ -z "$VIRTUAL_ENV" ] && . .venv/bin/activate
 venv_root=$(python3 -c 'import sys; print(sys.prefix)')
 venv_packages="$(ls -d "${venv_root}/lib/python"*"/site-packages")"
-{
-    git ls-files --others --exclude-standard
-    git ls-files --modified
-} | grep "^[a-z][a-z0-9-]*/walt/" | \
+
+file_changes="$({
+    git diff-index --name-status HEAD~10
+    git ls-files --others --exclude-standard | awk '{print "A " $1}'
+} | \
     grep -v "\.swp$" | \
     grep -v ":w$" | \
-    grep -v ":$" | \
-while read f
+    grep -v ":$"
+)"
+
+{
+    # handle files in <python-package>/walt/
+    echo "$file_changes" | grep "[[:space:]][a-z][a-z0-9-]*/walt/" | \
+    while read op f
+    do
+        mod_path=$(echo "$f" | sed -e 's/^[a-z0-9-]*\///')
+        venv_f="${venv_packages}/$mod_path"
+        echo "$op" "$f" "$venv_f"
+    done
+
+    # handle files in <python-package>/sh/
+    echo "$file_changes" | grep "[[:space:]][a-z][a-z0-9-]*/sh/" | \
+    while read op f
+    do
+        venv_f="${venv_root}/bin/$(basename "$f")"
+        echo "$op" "$f" "$venv_f"
+    done
+} | while read op f venv_f
 do
-	mod_path=$(echo "$f" | sed -e 's/^[a-z0-9-]*\///')
-	venv_f="${venv_packages}/$mod_path"
-    if diff -q "$f" "$venv_f" >/dev/null 2>&1
+    if [ "$op" = "D" ]
     then
-        # already modified / added in venv
-        continue
-    fi
-    if [ -e "$venv_f" ]
-    then
-        mode="updated"
+        if [ ! -e "$venv_f" ]
+        then
+            # already deleted
+            continue
+        fi
+        mode="deleted"
+        rm "$venv_f"
     else
-        mode="added"
+        if diff -q "$f" "$venv_f" >/dev/null 2>&1
+        then
+            # already modified / added in venv
+            continue
+        fi
+        if [ -e "$venv_f" ]
+        then
+            mode="updated"
+        else
+            mode="added"
+        fi
+        mkdir -p "$(dirname "$venv_f")"
+        cp "$f" "$venv_f"
     fi
-    mkdir -p "$(dirname "$venv_f")"
-    cp "$f" "$venv_f"
     echo $mode $venv_f
 done
